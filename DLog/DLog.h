f9125606253b26ce47df26652058e7ff23ba3a25
WangLei <lenonlenonwang@gmail.com>
2014-05-16 13:54:42 +0800

    a log for ios

---
+++ DLog/DLog.h
@@ -0,0 +1,12 @@
+
+//  Created by wang rui on 12-10-11.
+//  Copyright (c) 2012年 wang lei. All rights reserved.
+//
+#ifdef DEBUG
+#define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define DLog(...)
+#endif
+
+
+#endif
